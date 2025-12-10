cf15df7e6455c5dda4a4580eeb0904547aaf8cae
Maxim <89156031+MaximKoblyuk@users.noreply.github.com>
2025-12-10 12:51:26 +0100

    change set

--- main.tf
+++ main.tf
@@ -6,16 +6,23 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "azurerm" {
+    # Backend configuration is provided via init command or backend config file
+  }
 }
 
 provider "azurerm" {
   features {}
+  
+  # Authentication is handled via environment variables:
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "main" {
   name     = "rg-mycloud-dev"
-  location = "East US"
+  location = "westeurope"
 
   tags = {
     environment = "development"
